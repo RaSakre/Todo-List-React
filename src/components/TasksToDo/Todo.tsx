c565adfabfe37ddbd706575fb2420a2c826b4a8b
Ranel Nizamov <nizamov.ranel@yandex.ru>
2024-12-07 12:41:13 +0300

    edited todo added

--- src/components/TasksToDo/Todo.tsx
+++ src/components/TasksToDo/Todo.tsx
@@ -1,38 +1,71 @@
-import axios from 'axios'
-import { TodoContext } from '../../App';
-import { useContext, useState } from 'react';
-import { TodoUI } from './TodoUI';
+import axios from "axios";
+import { TodoContext } from "../../App";
+import { useContext, useState, useRef, useEffect } from "react";
+import { TodoUI } from "./TodoUI";
 
-export const Todo = (props:any) => {
-	const [task, setTask] = useState({
-		userId: 1,
-		id: props.id,
-		title: props.title,
-		completed: false,
-	})
-	const todoMeta = useContext(TodoContext);
-	const onDelete = () => {
-			axios.delete(`https://jsonplaceholder.typicode.com/todos/${props.id}?_limit=4`)
-			todoMeta.deleteTodo(props.id)
-	}
-	const onDone = () => {
-		fetch(`https://jsonplaceholder.typicode.com/todos/${props.id}?_limit=4`, {
-			method: 'PATCH',
-			body: JSON.stringify({
-				...task,
-				completed: true,
-			}),
-			headers: {
-				'Content-type': 'application/json; charset=UTF-8',
-			},
-		})
-		.then(res => res.json()).then(data => todoMeta.addDoneTodo(data))
-		todoMeta.deleteTodo(props.id)
-	}
-	return (
-		<TodoUI
-		task={task}
-		onDelete={onDelete}
-		onDone={onDone} />
-	)
-}
+export const Todo = (props: any) => {
+  const focusRef = useRef<HTMLInputElement | null>(null);
+  const [editing, setEditing] = useState(false);
+  const [editedTitle, setEditedTitle] = useState(props.title);
+  const [task, setTask] = useState({
+    userId: 1,
+    id: props.id,
+    title: props.title,
+    completed: false,
+  });
+  const todoMeta = useContext(TodoContext);
+  const onEdit = () => {
+    setEditing(true);
+    if (focusRef.current) {
+      focusRef.current.focus();
+    }
+  };
+  useEffect(() => {
+    if (editing && focusRef.current) {
+      focusRef.current.focus();
+    } else if (!editing && focusRef.current) {
+      focusRef.current.blur();
+    }
+  }, [editing]);
+  const onSave = () => {
+    setTask({
+      ...task,
+      title: editedTitle,
+    });
+    setEditing(false);
+  };
+  const onDelete = () => {
+    axios.delete(
+      `https://jsonplaceholder.typicode.com/todos/${props.id}?_limit=4`
+    );
+    todoMeta.deleteTodo(props.id);
+  };
+  const onDone = () => {
+    fetch(`https://jsonplaceholder.typicode.com/todos/${props.id}?_limit=4`, {
+      method: "PATCH",
+      body: JSON.stringify({
+        ...task,
+        completed: true,
+      }),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => todoMeta.addDoneTodo(data));
+    todoMeta.deleteTodo(props.id);
+  };
+  return (
+    <TodoUI
+      onSave={onSave}
+      editedTitle={editedTitle}
+      setEditedTitle={setEditedTitle}
+      editing={editing}
+      onEdit={onEdit}
+      focusRef={focusRef}
+      task={task}
+      onDelete={onDelete}
+      onDone={onDone}
+    />
+  );
+};
--- src/components/TasksToDo/TodoUI.tsx
+++ src/components/TasksToDo/TodoUI.tsx
@@ -1,23 +1,72 @@
-import styles from './Todo.module.css'
-import checkIcon from '../../images/Check.svg'
-import { ITodo } from '../../App'
-
-
+import styles from "./Todo.module.css";
+import checkIcon from "../../images/Check.svg";
+import editIcon from "../../images/edit.svg";
+import { ITodo } from "../../App";
+import { MutableRefObject } from "react";
 
 type Props = {
-	task: ITodo
-	onDone: () => void,
-	onDelete: () => void,
-}
+  task: ITodo;
+  focusRef: MutableRefObject<HTMLInputElement | null>;
+  onDone: () => void;
+  onDelete: () => void;
+  onEdit: () => void;
+  editing: boolean;
+  editedTitle: string;
+  setEditedTitle: (title: string) => void;
+  onSave: () => void;
+};
 
-export const TodoUI = ({task, onDone, onDelete}:Props) => {
-	return (
-		<div className={styles.todo}>
-		<h1 className={styles.todoText}>{task.title}</h1>
-		<div className={styles.todoImages}>
-		<img onClick={onDone} className={styles.doneButton} src={checkIcon} alt="" />
-		<img onClick={onDelete} className={styles.deleteButton} src='' alt="" />
-		</div>
-	</div>
-	)
-}
+export const TodoUI = ({
+  task,
+  onDone,
+  onDelete,
+  onEdit,
+  editing,
+  editedTitle,
+  setEditedTitle,
+  onSave,
+  focusRef,
+}: Props) => {
+  return (
+    <div className={styles.todo}>
+      {editing ? (
+        <div>
+          <input
+            ref={focusRef}
+            type="text"
+            value={editedTitle}
+            onChange={(e) => setEditedTitle(e.target.value)}
+            className={styles.editing}
+          />
+          <button className={styles.editingButton} onClick={onSave}>
+            Сохранить
+          </button>
+        </div>
+      ) : (
+        <>
+          <h1 className={styles.todoText}>{task.title}</h1>
+          <div className={styles.todoImages}>
+            <img
+              onClick={onDone}
+              className={styles.doneButton}
+              src={checkIcon}
+              alt=""
+            />
+            <img
+              onClick={onDelete}
+              className={styles.deleteButton}
+              src=""
+              alt=""
+            />
+            <img
+              onClick={onEdit}
+              className={styles.editButton}
+              src={editIcon}
+              alt=""
+            />
+          </div>
+        </>
+      )}
+    </div>
+  );
+};
